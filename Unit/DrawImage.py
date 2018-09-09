e8f090f43f03db63d7c8365d54c928f4ab26c7fc
libaoquan95 <1282676967@qq.com>
2018-09-09 16:34:15 +0800

    画图

--- Unit/DrawImage.py
+++ Unit/DrawImage.py
@@ -5,7 +5,7 @@ class DrawImage():
         self.title = title
         self.saveBasePath = saveBasePath
     def plotOne(self, y, label=""):
-        fig = plt.figure(facecolor='white')
+        fig = plt.figure(facecolor='white', figsize=(18,6))
         ax = fig.add_subplot(111)
         plt.plot(y, '-o', label=label)
         plt.title(self.title)
@@ -19,3 +19,28 @@ class DrawImage():
         plt.title(self.title)
         plt.legend()
         plt.show()
+    def plotTwoV2(self, y1, label1, y2, label2, y3, label3="", label4=""):
+        fig = plt.figure(facecolor='white')
+        ax = fig.add_subplot(111)
+        plt.plot(y1, '-o', label=label1)
+        plt.plot(y2, '-', label=label2)
+        for i in range(len(y3)):
+            if (y3[i] == 0):
+                plt.scatter(i, y2[i], color='k')
+            else:
+                plt.scatter(i, y2[i], color='orange')
+        plt.title(self.title)
+        plt.legend()
+        plt.show()
+
+def main():
+    a = []
+    b = []
+    for i in range(5):
+        a.append(i)
+        b.append(i*2)
+    c = [1,0,1,0,1]
+    di = DrawImage("test", "test")
+    di.plotTwoV2(a, "a", b, "b", c, "not hit", "hit")
+
+#main()
