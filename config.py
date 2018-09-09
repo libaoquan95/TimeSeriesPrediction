b97f048150b0d12e6a791166d5e37c2e5e068fd1
libaoquan95 <1282676967@qq.com>
2018-09-09 16:33:00 +0800

    变量配置

---
+++ config.py
@@ -0,0 +1,8 @@
+basePath = "Data/mart_waimai_crm/"
+indiactorTableName = "indicator_org_id_day_xxsfjye_958_table_prod"
+#indicatorKey = int(sys.argv[1])
+#baseTrainCount = int(sys.argv[2])
+
+epochs  = 10
+seqLen = 30
+sensitivity = 0.5
