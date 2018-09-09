672926b076bc1e5b2448a81ca524ac7e655668a9
libaoquan95 <1282676967@qq.com>
2018-09-09 16:32:27 +0800

    预测一天

--- ladderTraining.py
+++ ladderTraining.py
@@ -1,57 +1,52 @@
 import sys
 import time
+import numpy as np
 from Model.LSTM.lstm import lstm
 from Unit.DrawImage import DrawImage
 from Unit.Indicator import Indicator
 from Unit.Indicator import IndicatorTable
-
-def fun():
-    None
+import config
 
 if __name__ == '__main__':
     indicatorKey = int(sys.argv[1])
-
-    basePath = "Data/mart_waimai_crm/"
-    indiactorTableName = "indicator_org_id_day_xxsfjye_958_table_prod"
-    epochs  = 10
-    seq_len = 15
-    baseTrainCount = 300
-    Sensitivity = 0.1
+    baseTrainCount = int(sys.argv[2])
+    
+    epochs = config.epochs
+    seqLen = config.seqLen
+    sensitivity = config.sensitivity
 
     global_start_time = time.time()
     print('> Loading data... ')
-    indicatorTable = IndicatorTable(basePath+indiactorTableName)
+    indiactorTableName = config.basePath+config.indiactorTableName
+    indicatorTable = IndicatorTable(indiactorTableName)
     indicator = indicatorTable.getIndicatorByKey(indicatorKey)
     lstm = lstm()
-    windows = lstm.transformDataToWindows(indicator.df[indicator.valueName].tolist(), seq_len)
-    normaliseWindows, normaliseBase = lstm.normaliseWindows(windows)
-
-    X, Y = lstm.SplitDataV2(normaliseWindows)
-    print ('windoes size: %d' % len(X))
-    for i in range(len(X)-baseTrainCount):
-        trainCount = baseTrainCount + i
-        print ('> Data Loaded. Compiling(count=%d)...' % trainCount)
-        model = lstm.buildModel([1, seq_len, 100, 1])
+    indiactorData = lstm.normalisMinAndMax(indicator.df[indicator.valueName].tolist())
+    
+    print('> Data size = %d, bengin compiling...' %len(indiactorData))
+    testReal = []
+    testPred = []
+    testWindowBound = []
+    for i in range(baseTrainCount, len(indiactorData)):
+        windows = lstm.transformDataToWindows(indiactorData[:i], seqLen)
+        # 只用一个窗口，预测之后一天
+        xTrain, yTrain, xTest, yTest = lstm.SplitData(windows, 1)
+        print('> Compiling, data size =  %d...' % i)
+        model = lstm.buildModel([1, seqLen, 100, 1])
         model.fit(
-            X[:trainCount],
-            Y[:trainCount],
+            xTrain,
+            yTrain,
             batch_size=512,
             nb_epoch=epochs,
             validation_split=0.05)
-        #predicted = lstm.predictPointByPoint(model, X[trainCount:])     
+        predicted = lstm.predictPointByPoint(model, xTest)
 
-    '''
-    print('> Data Loaded. Compiling...')
-    model = lstm.buildModel([1, seq_len, 100, 1])
-    model.fit(
-        xTrain,
-        yTrain,
-        batch_size=512,
-        nb_epoch=epochs,
-        validation_split=0.05)
-    predicted = lstm.predictPointByPoint(model, xTest)        
+        testPred.append(predicted[0])
+        testReal.append(yTest[0])
+        tempXTest = np.reshape(xTest, (xTest.shape[1]))
+        testWindowBound.append([min(tempXTest), max(tempXTest)])
 
-    print('Training duration (s) : ', time.time() - global_start_time)
-    yTest = lstm.deNormaliseWindows(yTest, normaliseBase[len(yTrain):])
-    predicted = lstm.deNormaliseWindows(predicted, normaliseBase[len(yTrain):])
-    '''
+    hitScore, hitInfo = lstm.score(testReal, testPred, testWindowBound, sensitivity)
+    print ('score = %f' % hitScore)
+    DrawImage(indiactorTableName+"_"+str(indicatorKey), "img/").plotTwoV2(testReal, "True Date", testPred, "Predicted", hitInfo)
+    
