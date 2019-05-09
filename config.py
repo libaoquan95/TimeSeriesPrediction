d12d06d2e18ef82766bf8f704784d67149c78018
Li Baoquan <1282676967@qq.com>
2019-05-09 13:03:53 +0800

    Update config.py
    1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-basePath = "Data/mart_waimai_crm/"
-indiactorTableName = "indicator_org_id_day_xxsfjye_958_table_prod"
+basePath = "Data/path/"
+indiactorTableName = "table"
 #indicatorKey = int(sys.argv[1])
 #baseTrainCount = int(sys.argv[2])
 
